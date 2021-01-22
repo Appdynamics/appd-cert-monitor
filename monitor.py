a51ede9cf6d2fc3ec1c1f0cce38a72227b5804ad
Dustin Moorman <dustin.moorman@appdynamics.com>
2021-01-22 10:47:51 -0600

    silently allow exceptions by not reporting any metric

--- monitor.py
+++ monitor.py
@@ -13,14 +13,16 @@ for target in targets:
   port = target.get("port")
 
   context = ssl.create_default_context()
-  
-  with socket.create_connection((hostname, port)) as sock:
-      with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-          data = ssock.getpeercert()
-          expirationDateDT = datetime.datetime.strptime(data['notAfter'], '%b  %d %H:%M:%S %Y %Z') 
-  
-  now = datetime.datetime.today()
-  daysLeft = expirationDateDT - now
-  
-  DEFAULT_METRIC_PREFIX="name=Custom Metrics|CertMonitor|" + hostname + "|Days Until Cert Expiration,value=" + str(daysLeft.days)
-  print(DEFAULT_METRIC_PREFIX)
+  try:
+    with socket.create_connection((hostname, port)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            data = ssock.getpeercert()
+            expirationDateDT = datetime.datetime.strptime(data['notAfter'], '%b  %d %H:%M:%S %Y %Z') 
+    
+    now = datetime.datetime.today()
+    daysLeft = expirationDateDT - now
+    
+    DEFAULT_METRIC_PREFIX="name=Custom Metrics|CertMonitor|" + hostname + "|Days Until Cert Expiration,value=" + str(daysLeft.days)
+    print(DEFAULT_METRIC_PREFIX)
+  except:
+    pass
